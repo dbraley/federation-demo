1d3306b742fadd65394fd9671dbd28146618b2e9
Daniel Mai <daniel@dgraph.io>
2021-03-02 21:20:01 -0800

    Add Dgraph cluster for inventory service.

--- services/inventory/server/server.go
+++ services/inventory/server/server.go
@@ -1,42 +1,26 @@
 package main
 
 import (
-	"github.com/99designs/gqlgen/graphql/handler/extension"
-	"github.com/99designs/gqlgen/graphql/handler/transport"
-	"github.com/gorilla/websocket"
 	"log"
 	"net/http"
+	"net/http/httputil"
+	"net/url"
 	"os"
-	"time"
 
-	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
-	"github.com/marwan-at-work/federation-demo/services/inventory"
-	"github.com/vektah/gqlparser"
 )
 
 const defaultPort = "4004"
 
 func main() {
-	gqlparser.MustLoadSchema()
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort
 	}
-	srv := handler.New(inventory.NewExecutableSchema(inventory.Config{Resolvers: &inventory.Resolver{}}))
-	srv.AddTransport(transport.POST{})
-	srv.AddTransport(transport.Websocket{
-		KeepAlivePingInterval: 10 * time.Second,
-		Upgrader: websocket.Upgrader{
-			CheckOrigin: func(r *http.Request) bool {
-				return true
-			},
-		},
-	})
-	srv.Use(extension.Introspection{})
 
+	url, _ := url.Parse("http://localhost:8080")
 	http.Handle("/", playground.Handler("GraphQL playground", "/graphql"))
-	http.Handle("/graphql",srv)
+	http.Handle("/graphql", httputil.NewSingleHostReverseProxy(url))
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
