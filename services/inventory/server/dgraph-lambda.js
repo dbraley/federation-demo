a8280971b864479625703f02f0f94fd63a70b7e0
Daniel Mai <daniel@dgraph.io>
2021-03-15 18:12:15 -0700

    Update dgraph-lambda.js for shippingEstimate.

--- services/inventory/server/dgraph-lambda.js
+++ services/inventory/server/dgraph-lambda.js
@@ -1,9 +1,14 @@
-async function shippingEstimate({parents, args}) {
-    console.log(parents)
-    console.log(args)
-    return parents.map(p => parseInt(p.upc) + 1000)
+function shippingEstimate(object) {
+      // free for expensive items
+      if (object.price > 1000) return 0;
+      // estimate is based on weight
+      return object.weight * 0.5;
+}
+
+async function shippingEstimateResolver({parents}) {
+    return parents.map(p => shippingEstimate(p));
 }
 
 self.addMultiParentGraphQLResolvers({
-    "Product.shippingEstimate": shippingEstimate
+    "Product.shippingEstimate": shippingEstimateResolver
 })
